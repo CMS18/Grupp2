90d60e554b31f57ab4b45b0b450393526e31a489
Sven Kalkbrenner <sven.kalkbrenner@gmail.com>
2018-09-17 13:42:08 +0200

    Created Board

--- Uppgift1/Sudoku.cs
+++ Uppgift1/Sudoku.cs
@@ -8,15 +8,46 @@ namespace Uppgift1
 {
     class Sudoku
     {
-        private string table;
+        private string boardAsText;
+        char[,] newGameBoard = new char[9, 9];
+
 
         public string BoardAsText { get; set; }
 
-        public Sudoku(string table)
-        {
-            this.table = table;
+        private void Set(char[,] board)
+            {
+            int atChar = 0;
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int column = 0; column < board.GetLength(1); column++)
+                {
+                    BoardAsText += board[row,column]+ " ";
+                    atChar++;
+                    
+                }
+                BoardAsText += "\n";
+            }
         }
+        
 
+        public Sudoku(string boardAsText)
+        {
+            this.CreateBoard(boardAsText);
+        }
+        
+        public void CreateBoard(string table)
+        {
+            int atChar = 0;
+            for (int row = 0; row < newGameBoard.GetLength(0); row++)
+            {
+                for (int column = 0; column < newGameBoard.GetLength(1); column++)
+                {
+                    newGameBoard[row, column] = table[atChar];
+                    atChar++;
+                }
+            }
+            Set(newGameBoard);   
+        }
         public void Solve()
         {
 
