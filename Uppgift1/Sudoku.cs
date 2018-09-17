9b2f0c0c22faa36b55a3b64a3450d6f829305b9b
Freddie <freddie.runnding@yh.nackademin.se>
2018-09-17 12:06:48 +0200

    Added class and constructor

---
+++ Uppgift1/Sudoku.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift1
+{
+    class Sudoku
+    {
+        private string table;
+
+        public string BoardAsText { get; set; }
+
+        public Sudoku(string table)
+        {
+            this.table = table;
+        }
+
+        public void Solve()
+        {
+
+        }
+
+    }
+}
