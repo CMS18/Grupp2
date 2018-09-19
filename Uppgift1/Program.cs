967928b2128cc5c4399af9980e116b0b80c974ef
Freddie <freddie.runnding@yh.nackademin.se>
2018-09-19 23:23:09 +0200

    Refromatted and cleaned up code.

--- Uppgift1/Program.cs
+++ Uppgift1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Uppgift1
@@ -11,6 +12,7 @@ namespace Uppgift1
         static void Main(string[] args)
         {
             Sudoku game = new Sudoku("003006040800090050009204038028341009007000100500679280230508400080010006070400800"); 
+            
             
             // Console.WriteLine(game.BoardAsText);
             game.Solve();
--- Uppgift1/Sudoku.cs
+++ Uppgift1/Sudoku.cs
@@ -7,53 +7,45 @@ using System.Threading.Tasks;
 
 namespace Uppgift1
 {
-    class Sudoku
+    internal class Sudoku
     {
-        char[,] newGameBoard = new char[9, 9];
-
-
+        private readonly char[,] newGameBoard = new char[9, 9];
         public string BoardAsText { get; set; }
 
         private void FormatBoard(char[,] board)
         {
             BoardAsText = "";
-            for (int row = 0; row < board.GetLength(0); row++)
+            for (var row = 0; row < board.GetLength(0); row++)
             {
-                for (int column = 0; column < board.GetLength(1); column++)
-                {
-                    BoardAsText += board[row,column]+ "  ";
-                }
+                for (var column = 0; column < board.GetLength(1); column++) BoardAsText += board[row, column] + "  ";
+
                 BoardAsText += "\n";
             }
         }
-        
+
         public Sudoku(string boardAsText)
         {
-            this.CreateBoard(boardAsText);
+            CreateBoard(boardAsText);
         }
-        
+
         public void CreateBoard(string table)
         {
-            int atChar = 0;
-            for (int row = 0; row < newGameBoard.GetLength(0); row++)
-            {
-                for (int column = 0; column < newGameBoard.GetLength(1); column++)
-                {
-                    newGameBoard[row, column] = table[atChar++];
-                }
-            }
-            FormatBoard(newGameBoard);   
+            var atChar = 0;
+            for (var row = 0; row < newGameBoard.GetLength(0); row++)
+            for (var column = 0; column < newGameBoard.GetLength(1); column++) newGameBoard[row, column] = table[atChar++];
+
+            FormatBoard(newGameBoard);
         }
 
         public void Solve()
         {
             Console.Clear();
             Console.WriteLine(BoardAsText);
-            bool hasEmptyCell = false;
-            bool iGiveUp = true;
-            for (int row=0; row<newGameBoard.GetLength(0); row++)
+            var hasEmptyCell = false;
+            var iGiveUp = true;
+            for (var row = 0; row < newGameBoard.GetLength(0); row++)
             {
-                for (int column=0; column<newGameBoard.GetLength(1); column++)
+                for (var column = 0; column < newGameBoard.GetLength(1); column++)
                 {
                     // If this is the first  cell
                     if (row == 0 && column == 0)
@@ -61,44 +53,42 @@ namespace Uppgift1
                         hasEmptyCell = false;
                         iGiveUp = true;
                     }
+
                     // If this cell is empty
-                    if(newGameBoard[row, column] == '0')
+                    if (newGameBoard[row, column] == '0')
                     {
-                        printWithColor(row, column, '_', 0, ConsoleColor.Red);
+                        PrintWithColor(row, column, '_', 0, ConsoleColor.DarkRed);
                         hasEmptyCell = true;
-                        int availableNums = 0;
-                        char correctNum = '0';
+                        var availableNums = 0;
+                        var correctNum = '0';
+
                         // Check if 1-9 is possible in this cell
-                        for (int num=1; num<10; num++)
+                        for (var num = 1; num < 10; num++)
                         {
-                            char checkNum = (char) (num + 48);
-                            if ((IsNotInRow(checkNum, row)
-                                && IsNotInColumn(checkNum, column)) 
+                            var checkNum = (char) (num + 48);
+                            if (IsNotInRow(checkNum, row)
+                                && IsNotInColumn(checkNum, column)
                                 && IsNotInBox(checkNum, row, column))
                             {
                                 availableNums++;
                                 correctNum = checkNum;
                             }
                         }
+
                         // Check if there's only 1 available number for this cell
                         if (availableNums == 1)
                         {
                             newGameBoard[row, column] = correctNum;
-                            printWithColor(row, column, correctNum, 100, ConsoleColor.Green);                   
+                            PrintWithColor(row, column, correctNum, 100, ConsoleColor.DarkGreen);
                             iGiveUp = false;
                         }
                     }
-                    
+
                     // If this cell is the last and we are not giving up
-                    if (!iGiveUp && row == newGameBoard.GetLength(0)-1 && column == newGameBoard.GetLength(1)-1)
-                    {
-                        if (hasEmptyCell)
-                        {
-                            row = -1;  
-                        }
-                    }
+                    if (!iGiveUp && row == newGameBoard.GetLength(0) - 1 && column == newGameBoard.GetLength(1) - 1)
+                        if (hasEmptyCell) row = -1;
                 }
-                
+
                 // If we are on last row & we have an empty cell but couldn't add a number we stop looping
                 if (iGiveUp && row == newGameBoard.GetLength(0) - 1 && hasEmptyCell)
                 {
@@ -107,38 +97,44 @@ namespace Uppgift1
                     break;
                 }
             }
-            Console.SetCursorPosition(0, newGameBoard.GetLength(1)+1);
+
+            Console.SetCursorPosition(0, newGameBoard.GetLength(1) + 1);
             //FormatBoard(newGameBoard);
         }
+
         // Check if number is not in row
         private bool IsNotInRow(char num, int row)
         {
-            for(int column = 0; column < newGameBoard.GetLength(0); column++)
-            {
+            for (var column = 0; column < newGameBoard.GetLength(0); column++)
                 if (newGameBoard[row, column] == num) return false;
-            }
+
             return true;
         }
+
         // Check if number is not in column
         private bool IsNotInColumn(char num, int column)
         {
-            for (int row = 0; row < newGameBoard.GetLength(1); row++)
-                if (newGameBoard[row, column] == num) return false;
+            for (var row = 0; row < newGameBoard.GetLength(1); row++)
+                if (newGameBoard[row, column] == num)
+                    return false;
             return true;
         }
+
         // Check if number is not in box
         private bool IsNotInBox(char num, int row, int column)
         {
-            int startColumn = getStartFromThisBox(column);
-            int startRow = getStartFromThisBox(row);
+            var startColumn = GetStartFromThisBox(column);
+            var startRow = GetStartFromThisBox(row);
 
-            for (int r = startRow; r < startRow+3; r++)
-                for(int c = startColumn; c < startColumn+3; c++)
-                    if (newGameBoard[r, c] == num) return false;
+            for (var r = startRow; r < startRow + 3; r++)
+            for (var c = startColumn; c < startColumn + 3; c++)
+                if (newGameBoard[r, c] == num)
+                    return false;
             return true;
         }
+
         // Get the start position in a box (Upper left corner)
-        private int getStartFromThisBox(int element)
+        private static int GetStartFromThisBox(int element)
         {
             if (element < 3)
                 return 0;
@@ -147,8 +143,9 @@ namespace Uppgift1
             else
                 return 6;
         }
+
         // Print a number at correct position with specified color
-        private void printWithColor(int row, int column, char num, int sleepTime, ConsoleColor color)
+        private static void PrintWithColor(int row, int column, char num, int sleepTime, ConsoleColor color)
         {
             Console.SetCursorPosition(column * 3, row);
             Console.ForegroundColor = color;
