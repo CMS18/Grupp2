07f89e7b6bfaf9da4d389932cc2d96bc34fc7ef0
Freddie <freddie.runnding@yh.nackademin.se>
2018-09-24 15:47:52 +0200

    Added StringExtensions, LINQ-Instruderingsuppgifter

---
+++ LINQ-Instuderingsuppgifter/Extensions/StringExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Instuderingsuppgifter.Extensions
+{
+    public static class StringExtension
+    {
+        public static bool IsNumeric (this string text)
+        {
+            int result;
+            return int.TryParse(text, out result);
+        }
+
+        public static string UppercaseWordFirstLetter(this string text)
+        {
+
+            string[] words = text.Split(' ');
+            text = "";
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].First().ToString().ToUpper() + words[i].Substring(1, words[i].Length - 1) + " ";
+                text = string.Concat(text, words[i]);
+            }
+            
+            return text;
+        }
+        
+    }
+}
---
+++ LINQ-Instuderingsuppgifter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using LINQ_Instuderingsuppgifter.Extensions;
+
+namespace LINQ_Instuderingsuppgifter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter input: ");
+            var text = Console.ReadLine().UppercaseWordFirstLetter();
+
+            Console.WriteLine(text);
+        }
+    }
+}
