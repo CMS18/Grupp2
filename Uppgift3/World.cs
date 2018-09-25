8c7f909d0186e408ee28ad2f91338a5e03a60286
Sven Kalkbrenner <sven.kalkbrenner@gmail.com>
2018-09-25 10:56:34 +0200

    some more rooms

--- Uppgift3/World.cs
+++ Uppgift3/World.cs
@@ -11,13 +11,31 @@ namespace Uppgift3
         //List<Room> rooms = new List<Room>();
         public World()
         {
-            Room forest = new Room("Title", "Description"); //starting out in forest with axe and tree to solve the uppgift
+            Room forest = new Room("Dark Forest", "Description"); //starting out in forest with axe and tree to solve the uppgift
 
-            Room library = new Room("Title", "Description");
+            Room garden = new Room("Garden", "Description");
 
-            Room hall = new Room("Title", "Description");
+            Room garage = new Room("Garage", "Description");
+
+            Room library = new Room("Library", "Description");
+
+            Room hall = new Room("Hall", "Description");
+
+            Room kitchen = new Room("Kitchen", "Description");
+
+            Room masterBedroom = new Room("Master Bedroom", "Description");
+
+            Room toilet = new Room("Toilet", "Description");
+
+            Room bathRoom = new Room("Bathroom", "Description");
+
+            Room diningRoom = new Room("Dining Room", "Description");
+
+            Room guestRoom = new Room("Guest Room", "Description");
 
             library.East = hall;
+
+            forest.North = garden;
 
         }
     }
