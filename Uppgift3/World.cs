8b929ce1cb14931c16c4bae0c98bb394c288c111
Freddie <freddie.runnding@yh.nackademin.se>
2018-09-26 15:43:42 +0200

    Added description to a few rooms

--- Uppgift3/World.cs
+++ Uppgift3/World.cs
@@ -17,10 +17,9 @@ namespace Uppgift3
         public World()
         {
             // Rooms to use
-            Room forest =
-                new Room("Dark Forest",
-                    "This is a dark forest. "); //starting out in forest with axe and tree to solve the uppgift - words in UpperCase have a meaning
-            Room garden = new Room("Garden", "Description");
+            Room alleyway =
+                new Room("Dimmed alleyway", "The dingy alleyway reeks of the sour relics from a thousand take-away meals. "); 
+            Room smallStreet = new Room("Small Street", "The street is crowded with sketchy looking people. There's steam coming from a manhole in the middle of road. ");
             Room garage = new Room("Garage", "Description");
             Room library = new Room("Library", "Description");
             Room hallway = new Room("Hallway", "Description. ");
@@ -33,18 +32,18 @@ namespace Uppgift3
 
             // Exits
             library.East = hallway; //Move to hallway
-            forest.North = garden; //Move to garden
+            alleyway.North = smallStreet; //Move to street
             hallway.West = library; //Move to library
-            hallway.South = garden; //Move to garden
-            garden.South = forest; //Move to forest
-            garden.North = hallway; //move to hallway
+            hallway.South = smallStreet; //Move to street
+            smallStreet.South = alleyway; //Move to alleyway
+            smallStreet.North = hallway; //move to hallway
 
 
             hallway.AddItem(new Item(1, "Table", "This is a table made of cheap plastic.", 5));
             hallway.AddItem(new Item(1, "Chair", "This is an ugly chair.", 5));
 
             Weapon axe = new Weapon(2, "Axe", "Tool to chop things.", 5, 25);
-            Armor armor = new Armor(12, "Hat", "It's a fedora m'lady! *tips fedora*", 5, 1);
+            Armor armor = new Armor(12, "Hat", "It's a fedora. You feel the sudden urge to say 'm'lady'.", 5, 1);
             Npc glassUnicorn = new Npc(10, "Butt Stallion", "unicorn made of glass", true);
 
             hallway.AddItem(MakeToLegendary(armor));
