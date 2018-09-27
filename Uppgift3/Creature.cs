890b4a1b6692080197ad60c540250c45b89ca3d9
Tom Bergqvist <bergqvist.tom@gmail.com>
2018-09-27 13:05:17 +0200

    Added inventory to Creature

--- Uppgift3/Creature.cs
+++ Uppgift3/Creature.cs
@@ -13,23 +13,26 @@ namespace Uppgift3
         public string Description { get; set; }
         public bool Boss { get; set; }
 
+        private List<Item> inventory;
+
         public Creature(int id, string name, string description)
         {
             ID = id;
             Name = name;
             Description = description;
+            inventory = new List<Item>();
         }
 
         //TODO: Fix inventory for Creature
 
         public void additem(Item item)
         {
-
+            inventory.Add(item);
         }
 
         public void removeitem(Item item)
         {
-            
+            inventory.Remove(item);
         }
     }
 }
